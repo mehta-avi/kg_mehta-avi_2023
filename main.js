65092ff9e713a5b04fa254e474388f765edfd618
avim <avim@vt.edu>
2021-04-29 01:56:46 +0700

    enables user input functionality

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-var string = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-var numArray = [10,300,5, 69, 69420];
 
+var string = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
 var answers = [];
-for(var i = 0; i < numArray.length; i++){
-	var number = numArray[i];
+for(var i = 2; i < process.argv.length; i++){
+  var number = process.argv[i];
   var answer = "";
   while(number > 9){
     var u = number % 10;
     number = Math.floor(number/10);
     answer = string[u] + answer;
   }
-	answer = string[number] + answer;
-	answers.push(answer);
+  answer = string[number] + answer;
+  answers.push(answer);
 }
 
+
 function Concatenater (answers) {
-    var printStatement = "";
-    for(var i = 0; i < answers.length; i++){
-      if(i == answers.length - 1){
-      printStatement += answers[i];
-      }
-      else{
-      printStatement += answers[i] + ",";
-      }
+  var printStatement = "";
+  for(var i = 0; i < answers.length; i++){
+    if(i == answers.length - 1){
+    printStatement += answers[i];
     }
-    return printStatement;
+    else{
+    printStatement += answers[i] + ",";
+    }
   }
-  console.log(Concatenater(answers));
+  return printStatement;
+}
+console.log(Concatenater(answers));
